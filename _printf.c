90458de468c8ac889a94062c446575cd1053c718
Mvoi <mvoifranklin@gmail.com>
2023-11-14 19:32:54 +0300

    Update _printf.c - fix of implicit print_format declaration

--- _printf.c
+++ _printf.c
@@ -1,5 +1,26 @@
 #include "main.h"
 
+
+/**
+ * print_format - writes data formatted against some parameters
+ * @ap: the arguemnts list
+ * @specifier: identifies the data type
+ *
+ */
+int print_format(char specifier, va_list ap)
+{
+	int count = 0; 
+
+	/*handles char*/
+	if (specifier == 'c')
+		count =+ puts_char(va_arg(ap, int));
+
+	else if (specifier == 's')
+		count =+ puts_str(va_arg(ap, char *));
+	return (count);
+
+}
+
 /**
  * _printf - produces output according to a format
  *
@@ -49,24 +70,4 @@ int puts_str(char *str)
 		str++;
 	}
 	return count;
-}
-
-/**
- * print_format - writes data formatted against some parameters
- * @ap: the arguemnts list
- * @specifier: identifies the data type
- *
- */
-int print_format(char specifier, va_list ap)
-{
-	int count = 0; 
-
-	/*handles char*/
-	if (specifier == 'c')
-		count =+ puts_char(va_arg(ap, int));
-
-	else if (specifier == 's')
-		count =+ puts_str(va_arg(ap, char *));
-	return (count);
-
 }
