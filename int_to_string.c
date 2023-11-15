a73362d563e5b34698df108e80d33a4365070f2a
Mvoi <mvoifranklin@gmail.com>
2023-11-16 01:26:48 +0300

    Update int_to_string.c

--- int_to_string.c
+++ int_to_string.c
@@ -9,72 +9,59 @@
 */
 char* int_to_string(int num)
 {
-	char *str;
-	int i = 0;
-	int j = 0;
+    if (num == 0)
+    {
+        char* str = (char*)malloc(2 * sizeof(char));
+        if (!str)
+        {
+            perror("Memory allocation failed");
+            exit(EXIT_FAILURE);
+        }
+        str[0] = '0';
+        str[1] = '\0';
+        return str;
+    }
 
-	if (num == 0)
-	{
-		/*allocate mem for the string and null terminator*/
-		str = (char*)malloc(2 * sizeof(char));
+    int sign = (num < 0) ? -1 : 1;
+    num = abs(num); // Make num positive
 
-		if (!str)
-		{
-			perror("Memory allocataion failed");
-			exit(EXIT_FAILURE);
-		}
-		str[0] = '0';
-		str[1] = '\0';
+    // Calculate the number of digits
+    int digits = 1;
+    int temp = num;
+    while (temp >= 10)
+    {
+        digits++;
+        temp /= 10;
+    }
 
-		return (str);
-	}
+    char* str = (char*)malloc((digits + 1 + (sign == -1)) * sizeof(char));
+    if (!str)
+    {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
 
-	/*handle negative numbers*/
-	int sign = (num < 0) ? -1 : 1;
-	num = abs(num); /*make num positive*/
+    int i = 0;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        str[i++] = '0' + digit;
+        num /= 10;
+    }
 
-	/*extract digits and convert to characters*/
-	int digits = 1;
-	int temp = num;
+    if (sign == -1)
+        str[i++] = '-';
 
-	while (temp >= 10)
-	{
-		digit++;
-		temp /= 10;
-	}
+    // Reverse the string
+    int j = 0;
+    while (j < i / 2)
+    {
+        char temp = str[j];
+        str[j] = str[i - j - 1];
+        str[i - j - 1] = temp;
+        j++;
+    }
 
-	str = (char *)malloc((digits + 1 + (sign == -1)) * sizeof(char));
-	if(!str)
-	{
-		perror("Memory allocation Failed");
-		exit(EXIT_FAILURE);
-	}
-
-	int i = 0;
-	while (num > 0)
-	{
-		int digit = num % 10;
-		str[i++] = '0' + digit;
-		num /= 10;
-	}
-
-	if (sign == -1)
-	{
-		str[i++] = '-';
-	}
-
-	/*reverse string (since we built it from right to left)*/
-	while (j < i / 2)
-	{
-		char temp = str[j];
-
-		str[j] = str[i - j - 1];
-		str[i - j - 1] = temp;
-		j++;
-	}
-
-	/*add null terminator*/
-	str[i] = '\0';
-
-	return (str);
+    str[i] = '\0';
+    return str;
 }
