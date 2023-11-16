106494211d6c3d7033e1f9a6012d8a31975b12ea
Mvoi <mvoifranklin@gmail.com>
2023-11-16 23:37:28 +0300

    Update puts_digits.c - added %X handlers

--- puts_digits.c
+++ puts_digits.c
@@ -24,3 +24,29 @@ int puts_digits(long n, int base)
         return count + puts_digits(n % base, base);
     }
 }
+
+/**
+ * Prints an unsigned integer in caps hex format.
+ * @param value The unsigned integer value to be printed.
+ * Return: count of chars
+ */
+int puts_digit_caps(long n, int base)
+{
+    int count;
+    char *symbols;
+
+    symbols = "0123456789ABCDEF";
+    if (n < 0)
+    {
+        write(1, "-", 1);
+        return puts_digits(-n, base) + 1;
+    }
+    else if (n < base)
+        return puts_char(symbols[n]);
+
+    else
+    {
+        count = puts_digits(n / base, base);
+        return count + puts_digits(n % base, base);
+    }
+}
