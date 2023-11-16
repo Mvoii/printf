b6d681ea4f4f41ea0ee9f9f7409866461406c4af
Mvoi <mvoifranklin@gmail.com>
2023-11-16 23:27:00 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -49,29 +49,3 @@ int puts_integer(int value)
 	return (num_chars_printed);
 }
 
-/**
- * Prints an unsigned integer in binary format.
- * @param value The unsigned integer value to be printed.
- * Return: count of chars
- */
-int puts_digits(long n, int base)
-{
-    int count;
-    char *symbols;
-
-    symbols = "0123456789abcdef";
-    if (n < 0)
-    {
-        write(1, "-", 1);
-        return puts_digits(-n, base) + 1;
-    }
-    else if (n < base)
-        return puts_char(symbols[n]);
-
-    else
-    {
-        count = puts_digits(n / base, base);
-        return count + puts_digits(n % base, base);
-    }
-    
-}
