42ed05b1ea41c828aff5cdb065724367f539004d
Mvoi <mvoifranklin@gmail.com>
2023-11-16 22:11:58 +0300

    Update functions.c - u[dated puts_digits

--- functions.c
+++ functions.c
@@ -52,26 +52,26 @@ int puts_integer(int value)
 /**
  * Prints an unsigned integer in binary format.
  * @param value The unsigned integer value to be printed.
+ * Return: count of chars
  */
-int puts_binary(unsigned int value)
+int puts_digits(long n, int base)
 {
-	int count = 0;
+    int count;
+    char *symbols;
 
-	/*create a mask to extract each bit*/
-	unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
+    symbols = "0123456789abcdef";
+    if (n < 0)
+    {
+        write(1, "-", 1);
+        return puts_digits(-n, base) + 1;
+    }
+    else if (n < base)
+        return puts_char(symbols[n]);
 
-	unsigned int i = 0;
-
-	/*iterate through each bit and print it*/
-	while (mask > i)
-	{
-		if (value & mask)
-			count += puts_char('1');
-		else
-			count += puts_char('0');
-		mask >>= 1; /*shifts mask to the right*/
-		i++;
-	}
-
-	return (count);
+    else
+    {
+        count = puts_digits(n / base, base);
+        return count + puts_digits(n % base, base);
+    }
+    
 }
