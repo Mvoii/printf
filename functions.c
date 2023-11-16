7fc8c3e68dfd384d5d74dc163b3f859345f439f5
Mvoi <mvoifranklin@gmail.com>
2023-11-16 10:41:40 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -61,14 +61,13 @@ int puts_binary(unsigned int value)
 	unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
 
 	/*iterate through each bit and print it*/
-	count += puts_integer(mask);
-	/*while (mask > 0)
+	while (mask > 0)
 	{
 		if (value & mask)
 			count += puts_char('1');
 		else
 			count += puts_char('0');
 		mask >>= 1; /*shifts mask to the right*/
-	}*/
+	}
 	return (count);
 }
